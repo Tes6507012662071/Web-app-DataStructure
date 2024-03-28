8e4ae07c5819d48e258b74418466143caf907d12
Txss <s6507012662071@email.kmutnb.ac.th>
2024-03-28 23:14:10 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+
+// express app
+const app = express();
+
+// MongoDB connection URI
+const dbURI = "mongodb+srv://txss:admin@cluster0.cnhdyhu.mongodb.net/FashionStore?retryWrites=true&w=majority&appName=Cluster0";
+
+// Connect to MongoDB
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        // Start the server after successful connection
+        app.listen(3000, () => {
+            console.log('Server started on port 3000');
+        });
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err.message);
+        process.exit(1); // Exit the process on connection error
+    });
+
+// Register view engine
+app.set('view engine', 'ejs');
+
+// Middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
+
+// Routes
+app.get('/', (req, res) => {
+    res.redirect('/index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+// user routes
+app.use('/users', userRoutes);
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
+
+// Custom error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
---
+++ models/collection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+const Blog = mongoose.model('Blog', blogSchema);
+module.exports = Blog;
